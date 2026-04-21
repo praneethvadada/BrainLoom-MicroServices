0418cff9d817964d86981d7c686cc6594024b685
Praneeth Vadada <praneethvadada24@gmail.com>
2026-04-21 15:41:45 +0530

    pre-deploy

--- brainloom-frontend/lib/axios.ts
+++ brainloom-frontend/lib/axios.ts
@@ -2,7 +2,11 @@
 
 import axios from "axios";
 
-const BASE_URL = "http://localhost:4000/api/v1";
+const BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL ||
+  (process.env.NODE_ENV === "development"
+    ? "http://localhost:4000/api/v1"
+    : "/api/v1");
 
 export const api = axios.create({
   baseURL: BASE_URL,
